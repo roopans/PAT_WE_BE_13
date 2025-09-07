795d415ca110f4422d7401aa025929c418e2ebc7
roopans <sankarroopan@gmail.com>
2025-09-07 10:38:36 +0530

    Add files via upload

---
+++ Intro_to_Python/data_types_Intro_01.py
@@ -0,0 +1,23 @@
+# # 1. Data-types:
+#
+# str         # - when anything is enclosed with ' ' or " " or ''' ''' or """ """
+# int         # - non-decimal number from
+# float       # - decimal number from -infinity to +infinity.
+# bool        # - True or False
+# list        # - [1,2,3]
+# tuple       # - (1,2,3)
+# set         # - {1,2,3}
+# dict        # - {key1:value1 , key2:value2}
+# NONE
+
+#string
+print("printing string ")
+
+#integer
+print(2)
+print(-10)
+
+print(type(2))
+print(type('2'))
+
+print(type("string example"))

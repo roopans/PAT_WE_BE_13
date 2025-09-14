57ee786b0a3d92c799dc0415c6a85b54b8523832
Roopan <sankarroopan@gmail.com>
2025-09-14 12:39:47 +0530

    Update data type Conversion with comments

--- Intro_to_Python/data_types_Index_09.py
+++ Intro_to_Python/data_types_Index_09.py
@@ -6,22 +6,22 @@ print("First string:", test_str[0])
 print("Second string last character Index1:", test_str[-1])
 
 
-# Length of string
-liststring = len(test_str)
-print("Total Length given string:",liststring)
-
-
-# All String print using loop
-# for item in range(len(test_str));
-#     print(f"Item {item}:", {test_str[item]})
-for(index, character) in enumerate(test_str):
-    print(index, character)
-# String slicing
-print("Character of index 0 to 5:",test_str[0:5])
-print("Character of index last character:",test_str[1:-1])
-print("Character of index last character:",test_str[::])
+# # Length of string
+# liststring = len(test_str)
+# print("Total Length given string:",liststring)
+#
+#
+# # All String print using loop
+# # for item in range(len(test_str));
+# #     print(f"Item {item}:", {test_str[item]})
+# for(index, character) in enumerate(test_str):
+#     print(index, character)
+# # String slicing
+# print("Character of index 0 to 5:",test_str[0:5])
+# print("Character of index last character:",test_str[1:-1])
+# print("Character of index last character:",test_str[::])
 
 #palindrom string
-test = "madam"
+test = "testr"
 print(test[::1]) == print(test[::-1])
 
